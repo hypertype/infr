ba3a2f2c265d9917179cb55b48ad62fe5821b118
Andrey <andrey-ne@mail.ru>
2019-02-18 18:18:02 +0300

    feat(atuh): add auth service

--- common/index.ts
+++ common/index.ts
@@ -3,6 +3,8 @@ import {ApiService, ApiUrlInjectionToken} from "./api.service";
 import {IRequestService} from "./request.service";
 import {Container} from "@hypertype/core";
 import {WebSocketUrlInjectionToken} from "./i-web-socket.service";
+import {AuthApiService} from "./auth-api.service";
+
 export {BaseWebSocketService} from "./base-web-socket.service";
 
 export * from './logger';
@@ -17,6 +19,12 @@ InfrContainer.provide([
     {provide: ApiService, deps: [IRequestService, ApiUrlInjectionToken]},
     {provide: ApiUrlInjectionToken, useValue: 'http://localhost/api'},
     {provide: WebSocketUrlInjectionToken, useValue: 'http://localhost/inventory'},
+]);
+
+export const AuthContainer = new Container();
+AuthContainer.provide(InfrContainer);
+AuthContainer.provide([
+    {provide: ApiService, useClass: AuthApiService, deps: [IRequestService, ApiUrlInjectionToken]}
 ]);
 
 export * from './signalr';
